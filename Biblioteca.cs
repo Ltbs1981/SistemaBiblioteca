5cf01ea7f1b58dafb7884b24d00968555fe48857
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-09-16 17:03:45 -0300

    criada as classes, filme, revista, livro

---
+++ Biblioteca.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBiblioteca
+{
+    public class Biblioteca
+    {
+
+        public string  Titulo { get; set; }
+        public string  Ano { get; set; }
+        public int DiasDeAluguel { get; set; }
+        public double  ValorAluguel { get; set; }
+        public double   MultaDiaria{ get; set; }
+
+        public Biblioteca(string titulo, string ano, double valorAluguel, double multaDiaria, int diasDeAluguel)
+        {
+            Titulo = titulo;
+            Ano = ano;
+            DiasDeAluguel = diasDeAluguel;
+                        ValorAluguel = valorAluguel;
+            MultaDiaria = multaDiaria;  
+                    }
+public void AlugandoTitulo()
+        {
+            Console.WriteLine("Titulo alugado com sucesso!");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SistemaBiblioteca;
+
+Biblioteca revista = new Biblioteca("Revista Época", "2009", 3, 1250, 100);
+revista.AlugandoTitulo();
